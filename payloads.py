8e76c1814f677df6d94e0d7029874b7b3ed38b90
Girban <girban.b@proton.me>
2024-08-27 22:24:54 +0530

    Initial commit

---
+++ payloads.py
@@ -0,0 +1,33 @@
+boq_oldest_first = {
+  "param": {
+    "searchBid": "",
+    "searchType": "fullText"
+  },
+  "filter": {
+    "bidStatusType": "ongoing_bids",
+    "byType": "boq",
+    "highBidValue": "",
+    "byEndDate": {
+      "from": "",
+      "to": ""
+    },
+    "sort": "Bid-Start-Date-Oldest"
+  }
+}
+
+boq_newest_first = {
+  "param": {
+    "searchBid": "",
+    "searchType": "fullText"
+  },
+  "filter": {
+    "bidStatusType": "ongoing_bids",
+    "byType": "boq",
+    "highBidValue": "",
+    "byEndDate": {
+      "from": "",
+      "to": ""
+    },
+    "sort": "Bid-Start-Date-Latest"
+  }
+}
